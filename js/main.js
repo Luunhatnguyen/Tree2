afa9bfe21411d238c1b531880372073e4463c37a
Luunhatnguyen <nhatnguyen.01102001@gmail.com>
2022-09-24 16:02:04 +0700

    auto

---
+++ js/main.js
@@ -0,0 +1,44 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
+// CLICK
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const tabs = $$(".tab-item");
+const panes = $$(".tab-pane");
+const view = $$(".tab_pane");
+
+tabs.forEach((tab, index) => {
+  const pane = panes[index];
+  // const views = view[index];
+
+  tab.onclick = function () {
+    $(".tab-item.active").classList.remove("active");
+    $(".tab-pane.active").classList.remove("active");
+    // $(".tab_pane.active").classList.remove("active");
+
+    this.classList.add("active");
+    pane.classList.add("active");
+    // views.classList.add("active");
+  };
+});
