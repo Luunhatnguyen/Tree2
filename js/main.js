20ad7b95f32281731749125254becad1834ed1ce
Luunhatnguyen <nhatnguyen.01102001@gmail.com>
2022-09-24 16:52:14 +0700

    update nar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 10,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
